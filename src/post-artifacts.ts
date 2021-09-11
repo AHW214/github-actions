84c2f80e011b13ee6c2f1955d38022e9db0bf5e3
Adam Weider <ahw214@uchicago.edu>
2021-09-11 06:54:36 -0700

    help

--- src/post-artifacts.ts
+++ src/post-artifacts.ts
@@ -63,7 +63,7 @@ const findOutdatedComments = async (
 
     const matches = [...comment.body.matchAll(regexArtifact)];
     core.info(`${matches}`);
-    const artifactIds = matches.map((m) => Number(m[1])).filter(isNaN);
+    const artifactIds = matches.map((m) => Number(m[1]));
     core.info(`${artifactIds}`);
 
     return artifactIds.length > 0 && { commentId: comment.id, artifactIds };
