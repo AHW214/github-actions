55d2ee0b78675edc1c0b2c807da4c3f3269ff2cf
Adam Weider <ahw214@uchicago.edu>
2021-09-15 22:58:40 -0700

    thing

--- src/action/document-artifacts/run.ts
+++ src/action/document-artifacts/run.ts
@@ -20,8 +20,17 @@ const artifactView = (
   checkSuiteId: number,
   { name, id }: WorkflowRunArtifact,
 ): ArtifactView => ({
-  name: `${name}.zip`,
+  name,
   url: `https://github.com/${owner}/${repo}/suites/${checkSuiteId}/artifacts/${id}`,
+});
+
+const thing = (
+  owner: string,
+  repo: string,
+  checkSuiteId: number,
+  { name, id }: WorkflowRunArtifact,
+) => ({
+  [name]: `https://github.com/${owner}/${repo}/suites/${checkSuiteId}/artifacts/${id}`,
 });
 
 const run = async (
@@ -52,8 +61,11 @@ const run = async (
     artifactView(owner, repo, checkSuiteId, art),
   );
 
+  const things = artifacts.map((art) => thing(owner, repo, checkSuiteId, art));
+
   const rendered = mustache.render(template, {
     artifacts: artifactViews,
+    ...things,
   });
 
   core.info(rendered);
